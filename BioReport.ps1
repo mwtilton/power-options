4f24a31831be2d457cec9ac911d8e79051af42a1
Ct <charlietoast04@gmail.com>
2017-12-29 16:33:28 -0800

    Initial Commit
    Still need to import finalized version

---
+++ BioReport.ps1
@@ -0,0 +1,219 @@
+﻿#Set customer ID
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+$custID = [Microsoft.VisualBasic.Interaction]::InputBox("Enter customer ID","Customer ID")
+
+###########################################################################
+#Calendar
+
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+$form = New-Object Windows.Forms.Form 
+
+$form.Text = "Select a Date" 
+$form.Size = New-Object Drawing.Size @(243,230) 
+$form.StartPosition = "CenterScreen"
+
+$calendar = New-Object System.Windows.Forms.MonthCalendar 
+$calendar.ShowTodayCircle = $False
+$calendar.MaxSelectionCount = 90
+$form.Controls.Add($calendar) 
+
+$OKButton = New-Object System.Windows.Forms.Button
+$OKButton.Location = New-Object System.Drawing.Point(38,165)
+$OKButton.Size = New-Object System.Drawing.Size(75,23)
+$OKButton.Text = "OK"
+$OKButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$form.AcceptButton = $OKButton
+$form.Controls.Add($OKButton)
+
+$CancelButton = New-Object System.Windows.Forms.Button
+$CancelButton.Location = New-Object System.Drawing.Point(113,165)
+$CancelButton.Size = New-Object System.Drawing.Size(75,23)
+$CancelButton.Text = "Cancel"
+$CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$form.CancelButton = $CancelButton
+$form.Controls.Add($CancelButton)
+
+$form.Topmost = $True
+
+$result = $form.ShowDialog() 
+
+if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+{
+    $Date1 = $Calendar.SelectionStart
+    $DateFormat1 = $Date1.ToString("yyy-MM-dd")
+    $Date2 = $Calendar.SelectionEnd
+    $DateFormat2 = $Date2.ToString("yyy-MM-dd")
+    
+    Write-Host "Date selected: $DateFormat1 thru $DateFormat2"
+}
+
+Write-Host "Filename is set to: $custID-Report $DateFormat1 thru $DateFormat2.csv"  -ForegroundColor Green
+$outTxt = Out-File "C:\path\to\report\output\$custID - $DateFormat1 thru $DateFormat2.txt"
+$getOutTxt = Get-ChildItem $outTxt -Attributes archive  | % { $_.basename }
+
+######################
+#merger
+<#
+Start of merge process. Must have a customer ID and date in order to perform merge.
+#>
+
+$outMergedBioReport = "C:\path\to\report\output\$getOutTxt.csv"
+$TPoutMergedBioReport = Test-Path $outMergedBioReport
+if($TPoutMergedBioReport -ne $true)
+{
+    Write-Host "Running Biometric Report Merge Script" -ForegroundColor Magenta
+    Write-Warning "This may take some time to complete."
+    
+    $getBioReportCSVbasename = Get-ChildItem "C:\path\to\report\*.csv" -Attributes archive  | % { $_.basename }
+    foreach($file in $getBioReportCSVbasename)
+    {
+
+    #############################################
+    #CSV Merger
+        function Merge-CSVFiles { 
+        [cmdletbinding()] 
+        param( 
+            [string[]]$csvFiles, 
+            [string]$outMergedFile
+        ) 
+        $Output = @(); 
+        foreach($CSV in $csvFiles) { 
+            if(Test-Path $CSV) { 
+                Write-Warning "Starting merge process for: $file.csv" 
+                $FileName = [System.IO.Path]::GetFileName($CSV) 
+                $temp = Import-CSV -Path $CSV | select *, @{Expression={$FileName};Label="FileName"} 
+                $Output += $temp 
+                } 
+                else 
+                { 
+                Write-Warning "$CSV : No such file found" 
+                } 
+ 
+            }
+        $Output | select "Date/Time", "Zones / Devices / Slave Servers","Event Subtype","User","Workstation" | Export-Csv -Path $outMergedBioReport -nti
+        } 
+         
+    }
+        Merge-CSVFiles -csvFiles "C:\path\to\report\$file.csv"
+        
+        Write-Warning "Removing old $getOutTxt.txt"
+        Remove-Item "C:\path\to\report\output\*.txt"
+}
+Else
+{
+    Write-Warning "Else Warning."
+}
+
+
+#######################
+#.xlsx to .pdf
+<#
+$path = $outMergedBioReport
+$xlFixedFormat = "Microsoft.Office.Interop.Excel.xlFixedFormatType" -as [type] 
+$excelFiles = Get-ChildItem -Path $path -include *.xls, *.xlsx -recurse 
+$objExcel = New-Object -ComObject excel.application 
+$objExcel.visible = $false 
+foreach($wb in $excelFiles) 
+{ 
+ $filepath = Join-Path -Path $path -ChildPath ($wb.BaseName + ".pdf") 
+ $workbook = $objExcel.workbooks.open($wb.fullname, 3) 
+ $workbook.Saved = $true 
+"saving $filepath" 
+ $workbook.ExportAsFixedFormat($xlFixedFormat::xlTypePDF, $filepath) 
+ $objExcel.Workbooks.close() 
+} 
+$objExcel.Quit()
+#>
+
+
+####################### 
+#SQL
+<# 
+.SYNOPSIS 
+Runs a T-SQL script. 
+.DESCRIPTION 
+Runs a T-SQL script. Invoke-Sqlcmd2 only returns message output, such as the output of PRINT statements when -verbose parameter is specified 
+.INPUTS 
+None 
+    You cannot pipe objects to Invoke-Sqlcmd2 
+.OUTPUTS 
+   System.Data.DataTable 
+.EXAMPLE 
+Invoke-Sqlcmd2 -ServerInstance "MyComputer\MyInstance" -Query "SELECT login_time AS 'StartTime' FROM sysprocesses WHERE spid = 1" 
+This example connects to a named instance of the Database Engine on a computer and runs a basic T-SQL query. 
+StartTime 
+----------- 
+2010-08-12 21:21:03.593 
+.EXAMPLE 
+Invoke-Sqlcmd2 -ServerInstance "MyComputer\MyInstance" -InputFile "C:\MyFolder\tsqlscript.sql" | Out-File -filePath "C:\MyFolder\tsqlscript.rpt" 
+This example reads a file containing T-SQL statements, runs the file, and writes the output to another file. 
+.EXAMPLE 
+Invoke-Sqlcmd2  -ServerInstance "MyComputer\MyInstance" -Query "PRINT 'hello world'" -Verbose 
+This example uses the PowerShell -Verbose parameter to return the message output of the PRINT command. 
+VERBOSE: hello world 
+.NOTES 
+Version History 
+v1.0   - Chad Miller - Initial release 
+v1.1   - Chad Miller - Fixed Issue with connection closing 
+v1.2   - Chad Miller - Added inputfile, SQL auth support, connectiontimeout and output message handling. Updated help documentation 
+v1.3   - Chad Miller - Added As parameter to control DataSet, DataTable or array of DataRow Output type 
+ 
+function Invoke-Sqlcmd2 
+{ 
+    [CmdletBinding()] 
+    param( 
+    [Parameter(Position=0, Mandatory=$true)] [string]$ServerInstance, 
+    [Parameter(Position=1, Mandatory=$false)] [string]$Database, 
+    [Parameter(Position=2, Mandatory=$false)] [string]$Query, 
+    [Parameter(Position=3, Mandatory=$false)] [string]$Username, 
+    [Parameter(Position=4, Mandatory=$false)] [string]$Password, 
+    [Parameter(Position=5, Mandatory=$false)] [Int32]$QueryTimeout=600, 
+    [Parameter(Position=6, Mandatory=$false)] [Int32]$ConnectionTimeout=15, 
+    [Parameter(Position=7, Mandatory=$false)] [ValidateScript({test-path $_})] [string]$InputFile, 
+    [Parameter(Position=8, Mandatory=$false)] [ValidateSet("DataSet", "DataTable", "DataRow")] [string]$As="DataRow" 
+    ) 
+ 
+    if ($InputFile) 
+    { 
+        $filePath = $(resolve-path $InputFile).path 
+        $Query =  [System.IO.File]::ReadAllText("$filePath") 
+    } 
+ 
+    $conn=new-object System.Data.SqlClient.SQLConnection 
+      
+    if ($Username) 
+    { $ConnectionString = "Server={0};Database={1};User ID={2};Password={3};Trusted_Connection=False;Connect Timeout={4}" -f $ServerInstance,$Database,$Username,$Password,$ConnectionTimeout } 
+    else 
+    { $ConnectionString = "Server={0};Database={1};Integrated Security=True;Connect Timeout={2}" -f $ServerInstance,$Database,$ConnectionTimeout } 
+ 
+    $conn.ConnectionString=$ConnectionString 
+     
+    #Following EventHandler is used for PRINT and RAISERROR T-SQL statements. Executed when -Verbose parameter specified by caller 
+    if ($PSBoundParameters.Verbose) 
+    { 
+        $conn.FireInfoMessageEventOnUserErrors=$true 
+        $handler = [System.Data.SqlClient.SqlInfoMessageEventHandler] {Write-Verbose "$($_)"} 
+        $conn.add_InfoMessage($handler) 
+    } 
+     
+    $conn.Open() 
+    $cmd=new-object system.Data.SqlClient.SqlCommand($Query,$conn) 
+    $cmd.CommandTimeout=$QueryTimeout 
+    $ds=New-Object system.Data.DataSet 
+    $da=New-Object system.Data.SqlClient.SqlDataAdapter($cmd) 
+    [void]$da.fill($ds) 
+    $conn.Close() 
+    switch ($As) 
+    { 
+        'DataSet'   { Write-Output ($ds) } 
+        'DataTable' { Write-Output ($ds.Tables) } 
+        'DataRow'   { Write-Output ($ds.Tables[0]) } 
+    } 
+ 
+} 
+
+Invoke-Sqlcmd2
+
+#>
