bb8b0020d2fd48da92b736c728689354a5145256
Tilt <charlietoast04@gmail.com>
2018-01-05 01:36:20 -0800

    Initial Commit
    Event log and computer information based scripts.

---
+++ WinEventId-xml.ps1
@@ -0,0 +1,17 @@
+﻿# Grab the events from a DC            
+$Events = Get-WinEvent -FilterHashtable @{Logname='Application';Id=16384}            
+            
+# Parse out the event message data            
+ForEach ($Event in $Events) {            
+    # Convert the event to XML            
+    $eventXML = [xml]$Event.ToXml()            
+    # Iterate through each one of the XML message properties            
+    For ($i=0; $i -lt $eventXML.Event.EventData.Data.Count; $i++) {            
+        # Append these as object properties            
+        Add-Member -InputObject $Event -MemberType NoteProperty -Force -Name $eventXML.Event.EventData.Data[$i].name -Value $eventXML.Event.EventData.Data[$i].'#text'            
+    }            
+}            
+            
+# View the results with your favorite output method            
+$Events | Export-Csv .\events.csv            
+$Events | Select-Object * | Out-GridView 
